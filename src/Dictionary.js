8919c5b6d0e89e2743433acdb8c9ac6001dd4ce7
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-05-06 19:22:16 -0500

    Updated the search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,7 +15,7 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" />
+        <input type="search" onChange={handleKeywordChange} />
       </form>
     </div>
   );
