59ecb9207139838582ca7fe14d96a9e5497139a9
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-05-16 16:55:48 -0500

    Updated Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -14,7 +14,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt="image"
+                    alt="search"
                   />
                 </a>
               </div>
